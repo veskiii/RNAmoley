56ba81399c07df572f1fbcb7053d0ad353b4f409
HP <HP@LAPTOP-16PQ9653>
2024-11-23 14:55:38 +0100

    Commit 3

--- tools/scripts/PDBParser/Walking_sphere.py
+++ tools/scripts/PDBParser/Walking_sphere.py
@@ -8,63 +8,65 @@ from pathlib import Path
 
 parser = PDBParser()
 
-#pdb_file = Tutaj nazwa pliku_pdb
-#molecule = parser.get_structure('XNA',pdb_file)
-#radius = promień
-#desired = zaznaczony nukleotyd
+#sys.argv[1] = Nazwa analizowanego pliku
+#sys.argv[2] = folder do którego trafią wyniki
+#sys.argv[3] = wielkość promienia
+#sys.argv[4] = Liczba określa co który C-alfa będzie brany pod uwagę
 
 molecule = parser.get_structure('XNA',os.path.abspath(sys.argv[1]))
 
-radius = 16
+radius = int(sys.argv[3])
+Interval = int(sys.argv[4])
+Interval_count = Interval
 count = 1
-
-#file_count = []
-#for residue in molecule.get_residues():
-    #file_count.append(residue.id[1])
 
 path_to_folder = Path(sys.argv[2])
 if not path_to_folder.exists():
     path_to_folder.mkdir()
 
-print(os.path.abspath(path_to_folder))
-
-def Walking_sphere(count):
+def Walking_sphere(count, Interval_count, Interval):
     for main_atom in molecule.get_atoms():
         file = open(os.path.abspath(sys.argv[1]), 'r')
         if main_atom.id.strip("'") != "C1":
             continue
-        list = []
-        f = open(os.path.abspath(path_to_folder)+'/'+str(count)+'.pdb', 'w')
-        count+=1
-        for line in file:
-            if line[0:4] != 'ATOM' and line[0:6] != 'HETATM':
-                f.write(line)
-            else:
-                break
-        for supp_atom in molecule.get_atoms():
-            file.seek(0)
-            distance = np.linalg.norm(main_atom.coord - supp_atom.coord)
-            if distance <= radius:
+        else:
+            if Interval_count == Interval:
+                Interval_count = 1
+                list = []
+                f = open(os.path.abspath(path_to_folder)+'/'+str(count)+'.pdb', 'w')
+                count+=1
                 for line in file:
-                    if line[0:4].strip(" ") == 'ATOM' or line[0:6].strip(" ") == 'HETATM':
-                        if line[7:11].strip(" ") == str(supp_atom.serial_number):
-                            if line[22:26].strip(" ") not in list:
-                                list.append(line[22:26].strip(" "))
-                        else:
-                            continue
-        file.seek(0)
-        for line in file:
-            if line[22:26].strip(" ") in list:
-                f.write(line)
-        file.seek(0)
-        for line in file:
-            if line[0:6] == 'CONECT' or line[0:6] == 'MASTER' or line[0:3] == 'END':
-                f.write(line)
-        f.close()
-        file.seek(0)
+                    if line[0:4] != 'ATOM' and line[0:6] != 'HETATM':
+                        f.write(line)
+                    else:
+                        break
+                for supp_atom in molecule.get_atoms():
+                    file.seek(0)
+                    distance = np.linalg.norm(main_atom.coord - supp_atom.coord)
+                    if distance <= radius:
+                        for line in file:
+                            if line[0:4].strip(" ") == 'ATOM' or line[0:6].strip(" ") == 'HETATM':
+                                if line[7:11].strip(" ") == str(supp_atom.serial_number):
+                                    if line[22:26].strip(" ") not in list:
+                                        list.append(line[22:26].strip(" "))
+                                else:
+                                    continue
+                file.seek(0)
+                for line in file:
+                    if line[22:26].strip(" ") in list:
+                        f.write(line)
+                file.seek(0)
+                for line in file:
+                    if line[0:6] == 'CONECT' or line[0:6] == 'MASTER' or line[0:3] == 'END':
+                        f.write(line)
+                f.close()
+                file.seek(0)
+            else:
+                Interval_count += 1
+                continue
     file.close()
 
-Walking_sphere(count)
+Walking_sphere(count, Interval_count, Interval)
 
 #pool = mp.Pool(mp.cpu_count())
 #pool.map(Walking_sphere, file_count)
