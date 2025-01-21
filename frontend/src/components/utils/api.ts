93f08aa1bb2ae0c4fca4fdf2f85e3569132dce91
dawidgrajek1 <dawid.grajek@student.put.poznan.pl>
2025-01-21 15:32:53 +0100

    cors

--- frontend/src/components/utils/api.ts
+++ frontend/src/components/utils/api.ts
@@ -1,14 +1,14 @@
 import { Job } from "./types";
 
-const API_URL = "http://localhost:3000/api/v1";
-//const API_URL = "http://restapi/api/v1";
+// const API_URL = "http://localhost:3000/api/v1";
+const API_URL = "http://restapi:3001/api/v1";
 
 export const createJob = async (formData: FormData): Promise<any> => {
   const response = await fetch(`${API_URL}/jobs`, {
     method: "POST",
     body: formData,
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Origin": "*",
     },
   });
   if (!response.ok) {
@@ -20,7 +20,13 @@ export const createJob = async (formData: FormData): Promise<any> => {
 
 export async function fetchJobData(jobID: string | undefined, model: number = 1): Promise<Job> {
   try {
-    const response = await fetch(`${API_URL}/jobs/${jobID}/${model}`);
+    const response = await fetch(`${API_URL}/jobs/${jobID}/${model}`, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+    );
     if (!response.ok) {
       throw new Error(`Failed to fetch data: ${response.statusText}`);
     }
