81ab3b06a3f5197255237a266ac9a5bd0a81be75
veskiii <mikolaj.mlynarczyk@icloud.com>
2025-02-27 12:27:05 +0100

    comment usage of react-icons

--- frontend/src/components/common/homeIcon.tsx
+++ frontend/src/components/common/homeIcon.tsx
@@ -1,18 +1,18 @@
-import { FaHome } from "react-icons/fa";
+// import { FaHome } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const HomeIcon = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="justify-center flex flex-row items-center pl-2 text-md font-md z-[2000] font-medium cursor-pointer transition-colors hover:text-teal-600"
-        onClick={() => navigate("/")}
-
-        >
-            <FaHome style={{ width: '2em', height: '2em', marginRight: '0.5em'}} />
-                Submition Panel
-        </div>
-    );
+  return (
+    <div
+      className="justify-center flex flex-row items-center pl-2 text-md font-md z-[2000] font-medium cursor-pointer transition-colors hover:text-teal-600"
+      onClick={() => navigate("/")}
+    >
+      {/* <FaHome style={{ width: '2em', height: '2em', marginRight: '0.5em'}} /> */}
+      Submition Panel
+    </div>
+  );
 };
 
 export default HomeIcon;
