3d118a23022269c293978ab3443d906c883c99bd
veskiii <mikolaj.mlynarczyk@icloud.com>
2026-05-04 17:44:04 +0200

    bugfix: analyze button was disabled too often

--- frontend/src/components/panels/analysisPanel.tsx
+++ frontend/src/components/panels/analysisPanel.tsx
@@ -1169,7 +1169,7 @@ const Panel: React.FC = () => {
             <div className="mb-10">
               <button
                 className="rounded-md px-1 py-2 bg-moley-darkGreen text-sm font-semibold text-white shadow-xs hover:bg-moley-green focus-visible:outline-2 focus-visible:outline-offset-2 transition-colors disabled:bg-gray-300 disabled:cursor-not-allowed"
-                disabled={isDisabled}
+                disabled={getAllSelectedFragmentsGrouped().length === 0}
                 onClick={handleNavigate}
               >
                 Run analysis
